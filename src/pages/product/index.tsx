67098e8d0dfa5ec9382f315dc6651e7a7c0cacd1
yusufstawan <yusufsetiyawan45@gmail.com>
2023-10-14 23:53:11 +0700

    feat: csr & scheleton

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,12 +1,6 @@
+import ProductView from "@/views/Product";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-
-type productType = {
-  id: number;
-  name: string;
-  price: number;
-  size: string;
-};
 
 const ProductPage = () => {
   const [isLogin, setIsLogin] = useState(true);
@@ -27,12 +21,7 @@ const ProductPage = () => {
 
   return (
     <div>
-      <h1>Product Page</h1>
-      {products.map((product: productType) => (
-        <div key={product.id}>
-          <div>{product.name}</div>
-        </div>
-      ))}
+      <ProductView products={products as any} />
     </div>
   );
 };
---
+++ src/views/Product/index.tsx
@@ -0,0 +1,49 @@
+import styles from "./Product.module.scss";
+
+type productType = {
+  id: string;
+  name: string;
+  price: number;
+  image: string;
+  category: string;
+  map: Function;
+  length: number;
+};
+
+const ProductView = ({ products }: { products: productType }) => {
+  return (
+    <div className={styles.product}>
+      <h1 className={styles.product__title}>Product</h1>
+      <div className={styles.product__content}>
+        {products.length > 0 ? (
+          <>
+            {products.map((product: productType) => (
+              <div key={product.id} className={styles.product__content__item}>
+                <div className={styles.product__content__item__image}>
+                  <img src={product.image} alt={product.name} />
+                </div>
+                <h4 className={styles.product__content__item__name}>{product.name}</h4>
+                <p className={styles.product__content__item__category}>{product.category}</p>
+                <p className={styles.product__content__item__price}>
+                  {product.price.toLocaleString("id-ID", {
+                    style: "currency",
+                    currency: "IDR",
+                  })}
+                </p>
+              </div>
+            ))}
+          </>
+        ) : (
+          <div className={styles.product__content__skeleton}>
+            <div className={styles.product__content__skeleton__image} />
+            <div className={styles.product__content__skeleton__name} />
+            <div className={styles.product__content__skeleton__category} />
+            <div className={styles.product__content__skeleton__price} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProductView;
