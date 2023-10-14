f8cb74e940a0b28a080ebe47047bf926887a53ac
yusufstawan <yusufsetiyawan45@gmail.com>
2023-10-15 00:04:07 +0700

    feat: swr

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,6 +1,8 @@
 import ProductView from "@/views/Product";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import useSWR from "swr";
+import { fetcher } from "@/lib/swr/fetcher";
 
 const ProductPage = () => {
   const [isLogin, setIsLogin] = useState(true);
@@ -13,15 +15,17 @@ const ProductPage = () => {
     }
   }, []);
 
-  useEffect(() => {
-    fetch("api/product")
-      .then((res) => res.json())
-      .then((res) => setProducts(res.data));
-  }, []);
+  const { data, error, isLoading } = useSWR("api/product", fetcher);
+
+  // useEffect(() => {
+  //   fetch("api/product")
+  //     .then((res) => res.json())
+  //     .then((res) => setProducts(res.data));
+  // }, []);
 
   return (
     <div>
-      <ProductView products={products as any} />
+      <ProductView products={isLoading ? [] : (data.data as any)} />
     </div>
   );
 };
