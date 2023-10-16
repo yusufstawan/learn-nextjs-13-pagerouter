086f9ec02acacecd107f76192a6b7ace07de5135
yusufstawan <yusufsetiyawan45@gmail.com>
2023-10-16 14:57:07 +0700

    feat: ssg

---
+++ src/pages/product/static.tsx
@@ -0,0 +1,24 @@
+import ProductView from "@/views/Product";
+import { productType } from "@/types/product.type";
+
+const ProductPage = (props: { products: productType }) => {
+  const { products } = props;
+  return (
+    <div>
+      <ProductView products={products} />
+    </div>
+  );
+};
+
+export default ProductPage;
+
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:3000/api/product");
+  const response = await res.json();
+
+  return {
+    props: {
+      products: response.data,
+    },
+  };
+}
